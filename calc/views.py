2dde989c9c001c716d8c0bac06039df0be8a9446
Farhan Yousaf <farhanyousaf1996@gmail.com>
2022-08-29 15:23:08 -0400

    first commit

---
+++ calc/views.py
@@ -0,0 +1,14 @@
+from unicodedata import name
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+
+
+#Define the home view
+def home(request):
+    return HttpResponse('<h1>HELLLOOOO</h1>')
+
+
+def about(request):
+    return render(request, 'about.html')
